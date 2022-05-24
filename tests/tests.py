2384c6d0d57f03fcc25cdabb8bb87f3c97b4188d
Maurício Antunes <mauricio.abreua@gmail.com>
2022-05-24 01:15:21 -0300

    Feature and test are now working

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ import requests
 
 
 def test_create_todo():
-    response = requests.post("http://api:80/todos", data={"title": "buy some groceries"})
+    response = requests.post("http://api:80/todos", json={"title": "buy some groceries"})
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "buy some groceries"
