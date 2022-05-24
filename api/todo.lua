6565a2c7c69a841626e29a0d1ed71e1c2506753b
Maurício Antunes <mauricio.abreua@gmail.com>
2022-05-24 08:58:31 -0300

    Add GET and DELETE verbs

--- api/todo.lua
+++ api/todo.lua
@@ -4,9 +4,17 @@ local todolist = {}
 
 todolist.create = function(data, items)
   local uid = uuid()
-  local json_data = json.encode{title = data.title}
+  local json_data = json.encode{title = data.title, uid = uid}
   items:set(uid, json_data)
   return json_data
+end
+
+todolist.delete = function(uid, items)
+  items:delete(uid)
+end
+
+todolist.get = function(uid, items)
+  return items:get(uid)
 end
 
 return todolist
--- tests/tests.py
+++ tests/tests.py
@@ -7,3 +7,21 @@ def test_create_todo():
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "buy some groceries"
+
+def test_delete_todo():
+    response = requests.post("http://api:80/todos", json={"title": "buy some groceries"})
+    assert response.status_code == 200
+    data = response.json()
+
+    response = requests.delete(f"http://api:80/todos/{data['uid']}")
+    assert response.status_code == 204
+
+def test_get_todo():
+    response = requests.post("http://api:80/todos", json={"title": "buy some groceries"})
+    assert response.status_code == 200
+    data = response.json()
+
+    response = requests.get(f"http://api:80/todos/{data['uid']}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["title"] == "buy some groceries"
