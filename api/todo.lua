5f78eb1e15b66593b289151238d266cca546a924
Maurício Antunes <mauricio.abreua@gmail.com>
2022-05-24 10:26:50 -0300

    Test list todos

--- api/todo.lua
+++ api/todo.lua
@@ -17,4 +17,17 @@ todolist.get = function(uid, items)
   return items:get(uid)
 end
 
+todolist.list = function(items)
+  local list = {}
+  local uids = items:get_keys()
+  for _, uid in ipairs(uids) do
+    table.insert(list, json.decode(items:get(uid)))
+  end
+  return json.encode(list)
+end
+
+todolist.flush = function(items)
+  items:flush_all()
+end
+
 return todolist
--- tests/tests.py
+++ tests/tests.py
@@ -25,3 +25,15 @@ def test_get_todo():
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "buy some groceries"
+
+def test_list_todos():
+    requests.post("http://api:80/flush")
+
+    response = requests.post("http://api:80/todos", json={"title": "buy some groceries"})
+    assert response.status_code == 200
+    response = requests.post("http://api:80/todos", json={"title": "do homework"})
+    assert response.status_code == 200
+
+    response = requests.get("http://api:80/todos")
+    data = response.json()
+    assert len(data) == 2
