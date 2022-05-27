aa2e9ba70e6e421fe31aac85fe2f7d66d3bcacb2
Maurício Antunes <mauricio.abreua@gmail.com>
2022-05-27 11:13:39 -0300

    Prepare for Heroku deploy

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,7 @@ RUN apk add --no-cache git
 RUN luarocks install uuid
 RUN luarocks install rxi-json-lua
 RUN luarocks install lua-resty-cors
+
+COPY nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+
+CMD sed -i -e 's/$PORT/'"$PORT"'/g' /usr/local/openresty/nginx/conf/nginx.conf && nginx -g 'daemon off;'
--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,14 @@
+import os
 import requests
+
+
+SITE_URL = "http://api:8080"
+TODOS_ENDPOINT = f"{SITE_URL}/todos"
 
 
 def test_create_todo():
     response = requests.post(
-        "http://api:80/todos", json={"title": "buy some groceries"}
+        TODOS_ENDPOINT, json={"title": "buy some groceries"}
     )
     assert response.status_code == 200
     data = response.json()
@@ -13,57 +18,57 @@ def test_create_todo():
 
 def test_delete_todo():
     response = requests.post(
-        "http://api:80/todos", json={"title": "buy some groceries"}
+        TODOS_ENDPOINT, json={"title": "buy some groceries"}
     )
     assert response.status_code == 200
     data = response.json()
 
-    response = requests.delete(f"http://api:80/todos/{data['id']}")
+    response = requests.delete(f"{TODOS_ENDPOINT}/{data['id']}")
     assert response.status_code == 204
 
 
 def test_get_todo():
     response = requests.post(
-        "http://api:80/todos", json={"title": "buy some groceries"}
+        TODOS_ENDPOINT, json={"title": "buy some groceries"}
     )
     assert response.status_code == 200
     data = response.json()
 
-    response = requests.get(f"http://api:80/todos/{data['id']}")
+    response = requests.get(f"{TODOS_ENDPOINT}/{data['id']}")
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "buy some groceries"
 
 
 def test_list_todos():
-    requests.post("http://api:80/flush")
+    requests.post(f"{SITE_URL}/flush")
 
     response = requests.post(
-        "http://api:80/todos", json={"title": "buy some groceries"}
+       TODOS_ENDPOINT, json={"title": "buy some groceries"}
     )
     assert response.status_code == 200
-    response = requests.post("http://api:80/todos", json={"title": "do homework"})
+    response = requests.post(TODOS_ENDPOINT, json={"title": "do homework"})
     assert response.status_code == 200
 
-    response = requests.get("http://api:80/todos")
+    response = requests.get(TODOS_ENDPOINT)
     data = response.json()
     assert len(data) == 2
 
 
 def test_update_nonexistent_todo():
-    response = requests.patch(f"http://api:80/todos/1", json={"title": "do homework"})
+    response = requests.patch(f"{TODOS_ENDPOINT}/1", json={"title": "do homework"})
     assert response.status_code == 404
 
 
 def test_update_todo():
     response = requests.post(
-        "http://api:80/todos", json={"title": "buy some groceries"}
+        TODOS_ENDPOINT, json={"title": "buy some groceries"}
     )
     assert response.status_code == 200
     data = response.json()
 
     response = requests.patch(
-        f"http://api:80/todos/{data['id']}",
+        f"{TODOS_ENDPOINT}/{data['id']}",
         json={"title": "do homework", "completed": True},
     )
     assert response.status_code == 200
